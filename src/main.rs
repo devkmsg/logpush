62b4d7823cebbd3382bc781b7b1bb295303a499f
Andrew Thompson <netengr2009@gmail.com>
2017-07-07 22:08:55 -0400

    Some cli opts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use]
+extern crate clap;
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    // The YAML file is found relative to the current file, similar to how modules are found
+    let yaml = load_yaml!("../cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    let redis_host = matches.value_of("redis_host").unwrap_or("default.conf");
+    println!("Value for redis_host: {}", redis_host);
+
+    let redis_key = matches.value_of("redis_key").unwrap_or("default.conf");
+    println!("Value for redis_host: {}", redis_key);
+
+    let elasticsearch_host = matches.value_of("elasticsearch_host").unwrap_or("default.conf");
+    println!("Value for redis_host: {}", elasticsearch_host);
+
+    let index_pattern = matches.value_of("index_pattern").unwrap_or("default.conf");
+    println!("Value for redis_host: {}", index_pattern);
 }
