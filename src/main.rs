62b1082a2dde1315b28860045688ea994769ca4b
Andrew Thompson <netengr2009@gmail.com>
2017-07-14 17:50:21 -0400

    Adds Redis logic

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 extern crate clap;
 use clap::App;
 
+extern crate redis;
+use redis::Commands;
+use std::{thread, time};
+
+
 fn main() {
     // The YAML file is found relative to the current file, similar to how modules are found
     let yaml = load_yaml!("../cli.yml");
@@ -18,4 +23,34 @@ fn main() {
 
     let index_pattern = matches.value_of("index_pattern").unwrap_or("default.conf");
     println!("Value for index_pattern: {}", index_pattern);
+
+
+    let client = redis::Client::open(redis_host).unwrap();
+    let con = client.get_connection().unwrap();
+    let num_secs = 2;
+    let max_bulk_size = 8000;
+    let mut bulk = Vec::with_capacity(max_bulk_size);
+
+    loop {
+        //let now = std::time::SystemTime::now();
+        //println!("{:?}", now.duration_since(last_push));
+        let key: bool = con.exists(redis_key).unwrap();
+
+        if key == true { //FIXME: put in a timer so we can push evey x seconds
+            let line: redis::Value = con.rpop(redis_key).unwrap();
+            let line_value: redis::Value = redis::from_redis_value(&line).unwrap();
+
+            if bulk.len() >= max_bulk_size {
+                println!("Push bulk...");
+                bulk.truncate(0);
+
+            } else {
+                let last_push = std::time::SystemTime::now();
+                bulk.push(line_value);
+            }
+        } else {
+            println!("Cannot find key {}, sleeping for {} seconds", redis_key, num_secs);
+            thread::sleep(time::Duration::from_millis(1000 * num_secs));
+        }
+    }
 }
