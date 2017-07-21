6dac3cb6510a5a3d00d53fb01ed73a9a94521f92
Andrew Thompson <netengr2009@gmail.com>
2017-07-21 08:24:17 -0400

    Moving redis to new file

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,10 @@ extern crate clap;
 mod env;
 use self::env::ENV;
 
-extern crate redis;
-use redis::Commands;
+mod redis;
+use self::redis::Redis;
 
 use std::time::SystemTime;
-
-
-struct Redis;
-
-impl Redis {
-    pub fn new(host: &str) -> Result<redis::Connection, redis::RedisError> {
-        let client = try!(redis::Client::open(host));
-        let con = client.get_connection();
-        match con {
-            Ok(con) => Ok(con),
-            Err(e) => {
-                panic!("Error connecting to redis server: {}. {}", host, e);
-            },
-        }
-    }
-}
 
 
 fn main() {
@@ -32,7 +16,7 @@ fn main() {
     env.print();
 
 
-    let con = Redis::new(&env.redis_host).unwrap();
+    let con = Redis::new(env.redis_host, env.redis_key);
     let mut bulk = Vec::with_capacity(env.max_bulk_size);
 
     let mut last_bulk = SystemTime::now();
@@ -42,11 +26,8 @@ fn main() {
         let time_diff = now.duration_since(last_bulk).unwrap().as_secs();  //FIXME: not getting updated
         println!("time diff: {:?}", time_diff);
 
-        let key: bool = con.exists(&env.redis_key).unwrap();
-
-        if key == true { //FIXME: put in a timer so we can push evey x seconds
-            let line: redis::Value = con.rpop(&env.redis_key).unwrap();
-            let line_value: redis::Value = redis::from_redis_value(&line).unwrap();
+        if con.key_exists() == true {
+            let line_value = con.pop();
             bulk.push(line_value);
         }
 
---
+++ src/redis.rs
@@ -0,0 +1,39 @@
+extern crate redis;
+use self::redis::Commands;
+
+pub struct Redis {
+    pub connection: redis::Connection,
+    key: String,
+    host: String
+}
+
+impl Redis {
+    pub fn new(host: String, key: String) -> Redis {
+        Redis {
+            connection: Redis::connect(&host).unwrap(),
+            host: host,
+            key: key
+        }
+    }
+
+    fn connect(host: &String) -> Result<redis::Connection, redis::RedisError> {
+        let host: &str = host;
+        let client = try!(self::redis::Client::open(host));
+        let con = client.get_connection();
+        match con {
+            Ok(con) => Ok(con),
+            Err(e) => {
+                panic!("Error connecting to redis server: {}. {}", host, e);
+            },
+        }
+    }
+
+    pub fn pop(&self) -> redis::Value {
+        let line: self::redis::Value = self.connection.rpop(&self.key).unwrap();
+        self::redis::from_redis_value(&line).unwrap()
+    }
+
+    pub fn key_exists(&self) -> bool {
+        self.connection.exists(self.key.clone()).unwrap()
+    }
+}
